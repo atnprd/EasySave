c4d81e8c353964bd990d584ace74eb9bbf182e9b
Clarence <33149003+crunchi@users.noreply.github.com>
2019-11-21 17:18:34 +0100

    fonctional Saving method

---
+++ EasySave/Model/IBackup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Model
+{
+    interface IBackup
+    {
+        string name { get; set; }
+        string source_folder { get; set; }
+        string target_folder { get; set; }
+
+        void LaunchSave();
+
+        void LaunchSave(bool full_save);
+
+        void FullSave(DirectoryInfo di, string target_path);
+    }
+}
+
--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EasySave.Model;
+
 namespace EasySave
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IBackup save = new BackupDiff("test", "D:/projet/copy", "D:/projet/Test");
+            save.LaunchSave();
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter continuer");
+            Console.Read();
+
+            save.LaunchSave();
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter pour terminer le programe");
+            Console.Read();
         }
     }
 }
